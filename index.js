f26959bf7b97138f7ec24c9e763725385bf9e637
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-17 22:17:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-const express = require("express");
-const app = express();
-
-// Serve static files from the "public" folder
-app.use(express.static("public"));
-
-// Define a route for "/bot"
 app.get("/bot", (req, res) => {
-const bedrock = require('bedrock-protocol') 
-const client = bedrock.createClient({ 
-host: 'MoonLightSMPG2.aternos.me', 
-port: 64319, 
-version: '1.20.0', 
-username: 'Bot3', 
-offline: false
-})
-res.send('<h1>Bot is online</h1> <iframe width="560" height="315" src="https://www.youtube.com/embed/XvQxIsz30rE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <br> <iframe width="560" height="315" src="https://www.youtube.com/embed/9tLglpFAyIg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+  const bedrock = require('bedrock-protocol') 
+  const client = bedrock.createClient({ 
+    host: 'MoonLightSMPG2.aternos.me', 
+    port: 64319, 
+    version: '1.20.0', 
+    username: 'Bot3', 
+    offline: false
+  });
 
-});
+  const htmlResponse = `
+    <html>
+    <head>
+      <style>
+        body {
+          background-color: #f1f1f1;
+          font-family: Arial, sans-serif;
+          text-align: center;
+          margin: 0;
+          padding: 0;
+        }
 
-app.listen(3000, () => {
-  console.log('✅ Web is up');
+        h1 {
+          color: #333;
+          margin-top: 50px;
+        }
+
+        .video-container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+          margin-top: 30px;
+        }
+
+        .video-container iframe {
+          margin: 10px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Bot is online</h1>
+      <div class="video-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/XvQxIsz30rE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/9tLglpFAyIg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </body>
+    </html>
+  `;
+
+  res.send(htmlResponse);
 });
