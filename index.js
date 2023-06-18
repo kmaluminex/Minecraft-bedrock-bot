fbe5b178b2ae44c21b05ae37bfa8aecca762f14b
fancode343 <118608942+fancode343@users.noreply.github.com>
2023-06-18 17:09:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,84 +4,104 @@ const app = express();
 // Serve static files from the "public" folder
 app.use(express.static("public"));
 
+let botStatus = "Offline"; // Initialize the bot status as "Offline"
+let botName = "Local PC BOT"; // Set the bot name
+let client = null; // Initialize the client as null
+
 // Define a route for "/bot"
 app.get("/bot", (req, res) => {
-  const bedrock = require('bedrock-protocol') 
-  const client = bedrock.createClient({ 
-    host: 'MoonLightSMPG2.aternos.me', 
-    port: 64319, 
-    version: '1.20.0', 
-    username: 'Bot3', 
-    offline: true
-  });
+	if (!client) { // Only create a new client if it doesn't exist
+		const bedrock = require('bedrock-protocol')
+		client = bedrock.createClient({
+			host: 'MoonLightSMPG2.aternos.me',
+			port: 64319,
+			version: '1.20.0',
+			username: botName,
+			offline: true
+		});
 
-  const htmlResponse = `
+		// Set the bot status when it is online
+		client.on('spawn', () => {
+			botStatus = "Online";
+			// Emit an event to notify connected clients about the updated status
+			io.emit('botStatusUpdate', botStatus);
+		});
+
+		// Set the bot status when it goes offline
+		client.on('end', () => {
+			botStatus = "Offline";
+			client = null; // Reset the client
+
+			// Emit an event to notify connected clients about the updated status
+			io.emit('botStatusUpdate', botStatus);
+		});
+	}
+
+	const htmlResponse = `
     <html>
     <head>
       <style>
         body {
-          background-color: #f1f1f1;
+          background-color: #333; /* Change the background color to a cool gray */
           font-family: Arial, sans-serif;
           text-align: center;
           margin: 0;
           padding: 0;
+          color: #fff; /* Set the text color to white */
         }
 
         h1 {
-          color: #333;
+          color: #fff; /* Set the heading color to white */
           margin-top: 50px;
         }
 
-        .video-container {
-          display: flex;
-          flex-wrap: wrap;
-          justify-content: center;
-          margin-top: 30px;
-        }
-
-        .video-container iframe {
-          margin: 10px;
+        .bot-status {
+          font-size: 24px;
+          margin-top: 20px;
         }
       </style>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.3.2/socket.io.js"></script>
       <script>
+        const socket = io();
+
+        // Update bot status value when receiving an update event
+        socket.on('botStatusUpdate', (status) => {
+          document.getElementById('bot-status').innerText = status;
+        });
+
+        // Initial update of bot status value when the page loads
         document.addEventListener('DOMContentLoaded', () => {
-          const statusElement = document.getElementById('bot-status');
-
-          // Update the bot status dynamically
-          function updateBotStatus(status) {
-            statusElement.textContent = status;
-          }
-
-          // Perform an asynchronous request to check the bot status
-          async function checkBotStatus() {
-            try {
-              const response = await fetch('/bot/status'); // Assuming you have a separate route to fetch the bot status
-              const data = await response.json();
-              updateBotStatus(data.status);
-            } catch (error) {
-              console.error('Error fetching bot status:', error);
-              updateBotStatus('Error');
-            }
-          }
-
-          // Call the function initially and set an interval to periodically check the bot status
-          checkBotStatus();
-          setInterval(checkBotStatus, 5000); // Adjust the interval as needed
+          socket.emit('pageLoad');
         });
       </script>
     </head>
     <body>
-      <h1>Bot is online</h1>
-      <div class="video-container">
-        Bot status: <span id="bot-status"></span>
+      <h1>Bot Status</h1>
+      <div class="bot-status">
+        Bot: ${botName}<br>
+        Status: <span id="bot-status">${botStatus}</span>
       </div>
     </body>
     </html>
   `;
 
-  res.send(htmlResponse);
+	res.send(htmlResponse);
 });
 
-app.listen(3000, () => {
-  console.log('✅ Web is up');
+const server = app.listen(3000, () => {
+	console.log('✅ Web is up');
+});
+
+const io = require('socket.io')(server);
+
+// Listen for connection events from clients
+io.on('connection', (socket) => {
+	// Send the initial status value when a client connects
+	socket.emit('botStatusUpdate', botStatus);
+
+	// Handle page load event from clients
+	socket.on('pageLoad', () => {
+		// Send the initial status value when the page loads
+		socket.emit('botStatusUpdate', botStatus);
+	});
 });
